a95a1a82c7f4101bee9c97a35a4d44afb20f5f67
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2024-09-06 12:54:49 -0400

    fix subOracle mapping

--- src/oracle.ts
+++ src/oracle.ts
@@ -1,15 +1,24 @@
-import { Oracle } from '../generated/schema'
+import { Address } from '@graphprotocol/graph-ts'
+import { Oracle as OracleStore, SubOracle as SubOracleStore } from '../generated/schema'
 import { SubOracle } from '../generated/templates'
 import { OracleUpdated } from '../generated/templates/Oracle/Oracle'
 
 // When an Oracle is updated, we need to update the subOracle field
 export function handleOracleUpdated(event: OracleUpdated): void {
-  const entity = Oracle.load(event.address)
-  if (entity == null) throw new Error('Oracle not found')
+  const oracleEntity = OracleStore.load(event.address)
+  if (oracleEntity == null) throw new Error('Oracle not found')
 
-  entity.subOracle = event.params.newProvider
-  entity.save()
+  oracleEntity.subOracle = event.params.newProvider
+  oracleEntity.save()
 
-  // Create a new datasource template for the new subOracle
-  SubOracle.create(event.params.newProvider)
+  // Create SubOracle entity for Oracle
+  let subOracleEntity = SubOracleStore.load(oracleEntity.subOracle)
+  if (!subOracleEntity) {
+    subOracleEntity = new SubOracleStore(oracleEntity.subOracle)
+    subOracleEntity.oracle = oracleEntity.id
+    subOracleEntity.save()
+
+    // Create Template for SubOracle
+    SubOracle.create(Address.fromBytes(oracleEntity.subOracle))
+  }
 }
